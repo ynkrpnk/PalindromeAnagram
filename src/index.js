a0763dbeab32e6e509a523ed3a2286ed50bbf65d
ynkrpnk <ankarpenko35@gmail.com>
2026-04-25 23:51:42 +0300

    sketches

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 'use strict';
+
+
+const separator = '';
+const palindromString = (str, separator) => str.split(separator).reverse().join(separator)
+
+const reverseLine = (str) =>{
+    const separator = '';
+    const strWithoutGaps = str.toLowerCase().replaseAll('', separator);
+
+}
+
+
+console.log(palindromString('mama', separator));
